fa80567fc5ddbed02aa597f3d824a42a89a0ded9
Puneet Jindal <puneet.jindal@ephesoft.com>
2016-05-29 23:46:13 +0530

    updates on python scikit-learn and credit risk modeling

--- credit risk modelling.R
+++ credit risk modelling.R
@@ -138,3 +138,33 @@ pred_cutoff_15 <- ifelse(predictions_all_full > 0.15,1,0)
 
 # Construct a confusion matrix
 table(test_set$loan_status,pred_cutoff_15)
+
+# Fit the logit, probit and cloglog-link logistic regression models
+log_model_logit <- glm(loan_status ~ age + emp_cat + ir_cat + loan_amnt,
+                       family = binomial(link = logit), data = training_set)
+log_model_probit <- glm(loan_status ~ age + emp_cat + ir_cat + loan_amnt,
+                        family = binomial(link = probit), data = training_set)
+
+log_model_cloglog <- glm(loan_status ~ age + emp_cat + ir_cat + loan_amnt,
+                         family = binomial(link = cloglog), data = training_set) 
+
+# Make predictions for all models using the test set
+predictions_logit <- predict(log_model_logit, newdata = test_set, type = "response")
+predictions_probit <- predict(log_model_probit, newdata = test_set, type = "response")
+predictions_cloglog <- predict(log_model_cloglog, newdata = test_set, type = "response")
+
+# Use a cut-off of 14% to make binary predictions-vectors
+cutoff <- 0.14
+class_pred_logit <- ifelse(predictions_logit > cutoff, 1, 0)
+class_pred_probit <- ifelse(predictions_probit > cutoff, 1, 0)
+class_pred_cloglog <- ifelse(predictions_cloglog > cutoff, 1, 0)
+
+# Make a confusion matrix for the three models
+tab_class_logit <- table(true_val,class_pred_logit)
+tab_class_probit <- table(true_val,class_pred_probit)
+tab_class_cloglog <- table(true_val,class_pred_cloglog)
+
+# Compute the classification accuracy for all three models
+acc_logit <- sum(diag(tab_class_logit)) / nrow(test_set)
+acc_probit <- sum(diag(tab_class_probit)) / nrow(test_set)
+acc_cloglog <- sum(diag(tab_class_cloglog)) / nrow(test_set)
